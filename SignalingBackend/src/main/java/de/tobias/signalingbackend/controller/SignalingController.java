46c36a668ac1c1ba3d332c1571e7d633efeb04e7
Weiland <tobias.weiland@MB0049.local>
2021-10-22 11:53:58 +0200

    create basic ´request server is running´ in backend controller

--- SignalingBackend/src/main/java/de/tobias/signalingbackend/controller/SignalingController.java
+++ SignalingBackend/src/main/java/de/tobias/signalingbackend/controller/SignalingController.java
@@ -1,4 +1,18 @@
 package de.tobias.signalingbackend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("/api")
 public class SignalingController {
+
+    @GetMapping("/conncetion")
+    public ResponseEntity<String> connection(){
+        return ok("Server is running");
+    }
 }
