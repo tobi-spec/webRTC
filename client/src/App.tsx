c178c0cbd2a26a8050c19f834927c01528a9be67
Weiland <tobias.weiland@MB0049.local>
2021-10-22 15:01:25 +0200

    Setup Client and RTCDataChannel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+const conn = new WebSocket("ws://localhost:8080/socket");
+
+const send = (message: String) => {
+  conn.send(JSON.stringify(message))
+};
+
+const peerConnection = new RTCPeerConnection()
+
+const dataChannel = peerConnection.createDataChannel("dataChannel", );
+
+dataChannel.onerror = function (error){
+  console.log("Error", error)
+}
+
+dataChannel.onclose = function () {
+  console.log("Data channel is closed")
+}
+
+
 function App() {
-  return (<section></section>);
+  return (<section> Hello webRTC </section>);
 }
 
 export default App;
