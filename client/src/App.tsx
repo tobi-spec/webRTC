b5837e3d0bc3f3670a19ff0d755b24d4b9f65a9b
Weiland <tobias.weiland@MB0049.local>
2021-10-22 16:57:44 +0200

    rollback to status of 76d81a3bb44495f5f51ad6b8c36061c5b331fffa, set back last two previouse commits

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,7 @@
 import React from 'react';
 
-const conn = new WebSocket("ws://localhost:8080/socket");
-
-const send = (message:string)=> {
-  conn.send(JSON.stringify(message))
-};
-
-const peerConnection = new RTCPeerConnection()
-
-const dataChannel = peerConnection.createDataChannel("dataChannel", );
-
-dataChannel.onerror = function(error){
-  console.log("Error", error)
-}
-
-dataChannel.onclose = function () {
-  console.log("Data channel is closed")
-}
-
-peerConnection.createOffer(function (offer){
-  send({
-    event : "offer",
-    data : offer
-  });
-  peerConnection.setLocalDescription(offer);
-})
-
-
 function App() {
-  return (<section> Hello webRTC </section>);
+  return (<section></section>);
 }
 
 export default App;
