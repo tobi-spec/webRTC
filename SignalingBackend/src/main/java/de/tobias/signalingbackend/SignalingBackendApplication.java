3bdbc1400cc202213be3a8fb62dbdd2624ca38d8
Weiland <tobias.weiland@MB0049.local>
2021-10-22 11:47:45 +0200

    init spring boot with gradle backend

---
+++ SignalingBackend/src/main/java/de/tobias/signalingbackend/SignalingBackendApplication.java
@@ -0,0 +1,13 @@
+package de.tobias.signalingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignalingBackendApplication.class, args);
+    }
+
+}
---
+++ SignalingBackend/src/test/java/de/tobias/signalingbackend/SignalingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tobias.signalingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
